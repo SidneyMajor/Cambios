4fbc23d2449095ce716680187ec9cc5bffc2625a
Sidney Major <Sidney.Abreu.Major@formandos.cinel.pt>
2020-04-03 09:56:18 +0100

    First project to use API

---
+++ Cambios/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Cambios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxValor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ComboBoxOrigem = new System.Windows.Forms.ComboBox();
+            this.ComboBoxDestino = new System.Windows.Forms.ComboBox();
+            this.LabelResultado = new System.Windows.Forms.Label();
+            this.ButtonConverter = new System.Windows.Forms.Button();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ButtonTroca = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Moeda de Origem:";
+            // 
+            // TextBoxValor
+            // 
+            this.TextBoxValor.Location = new System.Drawing.Point(94, 41);
+            this.TextBoxValor.Name = "TextBoxValor";
+            this.TextBoxValor.Size = new System.Drawing.Size(399, 22);
+            this.TextBoxValor.TabIndex = 2;
+            this.TextBoxValor.TextChanged += new System.EventHandler(this.TextBoxValor_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Moeda de Destino:";
+            // 
+            // ComboBoxOrigem
+            // 
+            this.ComboBoxOrigem.FormattingEnabled = true;
+            this.ComboBoxOrigem.Location = new System.Drawing.Point(221, 90);
+            this.ComboBoxOrigem.Name = "ComboBoxOrigem";
+            this.ComboBoxOrigem.Size = new System.Drawing.Size(294, 24);
+            this.ComboBoxOrigem.TabIndex = 4;
+            // 
+            // ComboBoxDestino
+            // 
+            this.ComboBoxDestino.FormattingEnabled = true;
+            this.ComboBoxDestino.Location = new System.Drawing.Point(221, 138);
+            this.ComboBoxDestino.Name = "ComboBoxDestino";
+            this.ComboBoxDestino.Size = new System.Drawing.Size(294, 24);
+            this.ComboBoxDestino.TabIndex = 5;
+            // 
+            // LabelResultado
+            // 
+            this.LabelResultado.AutoSize = true;
+            this.LabelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelResultado.Location = new System.Drawing.Point(127, 197);
+            this.LabelResultado.Name = "LabelResultado";
+            this.LabelResultado.Size = new System.Drawing.Size(354, 18);
+            this.LabelResultado.TabIndex = 6;
+            this.LabelResultado.Text = "Escolha um valor, moeda de origem e destino";
+            // 
+            // ButtonConverter
+            // 
+            this.ButtonConverter.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ButtonConverter.Enabled = false;
+            this.ButtonConverter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonConverter.Location = new System.Drawing.Point(520, 28);
+            this.ButtonConverter.Name = "ButtonConverter";
+            this.ButtonConverter.Size = new System.Drawing.Size(114, 44);
+            this.ButtonConverter.TabIndex = 7;
+            this.ButtonConverter.Text = "Converter";
+            this.ButtonConverter.UseVisualStyleBackColor = false;
+            this.ButtonConverter.Click += new System.EventHandler(this.ButtonConverter_Click);
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Location = new System.Drawing.Point(12, 274);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(46, 17);
+            this.LabelStatus.TabIndex = 8;
+            this.LabelStatus.Text = "status";
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(466, 265);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(154, 23);
+            this.ProgressBar1.TabIndex = 9;
+            // 
+            // ButtonTroca
+            // 
+            this.ButtonTroca.BackColor = System.Drawing.Color.Transparent;
+            this.ButtonTroca.BackgroundImage = global::Cambios.Properties.Resources.icons8_change_96px;
+            this.ButtonTroca.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonTroca.Enabled = false;
+            this.ButtonTroca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTroca.Location = new System.Drawing.Point(533, 90);
+            this.ButtonTroca.Name = "ButtonTroca";
+            this.ButtonTroca.Size = new System.Drawing.Size(54, 68);
+            this.ButtonTroca.TabIndex = 10;
+            this.ButtonTroca.UseVisualStyleBackColor = false;
+            this.ButtonTroca.Click += new System.EventHandler(this.ButtonTroca_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(646, 300);
+            this.Controls.Add(this.ButtonTroca);
+            this.Controls.Add(this.ProgressBar1);
+            this.Controls.Add(this.LabelStatus);
+            this.Controls.Add(this.ButtonConverter);
+            this.Controls.Add(this.LabelResultado);
+            this.Controls.Add(this.ComboBoxDestino);
+            this.Controls.Add(this.ComboBoxOrigem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBoxValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Câmbios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxValor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ComboBoxOrigem;
+        private System.Windows.Forms.ComboBox ComboBoxDestino;
+        private System.Windows.Forms.Label LabelResultado;
+        private System.Windows.Forms.Button ButtonConverter;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.ProgressBar ProgressBar1;
+        private System.Windows.Forms.Button ButtonTroca;
+    }
+}
+
---
+++ Cambios/Form1.cs
@@ -0,0 +1,172 @@
+﻿
+
+namespace Cambios
+{
+
+    using Modelos;
+    using Cambios.Servicos;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.Drawing;
+
+    public partial class Form1 : Form
+    {
+        #region Atributos
+        private List<Rate> Rates;
+        private NetworkService networkService;
+
+        private ApiService apiService;
+
+        private DialogService dialogService;
+
+        private DataService dataService;
+        #endregion
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            networkService = new NetworkService();
+            apiService = new ApiService();
+            dialogService = new DialogService();
+            dataService = new DataService();
+            LoadRates();
+        }
+
+        private async void LoadRates()
+        {
+            bool load;
+           
+            LabelResultado.Text = "A atualizar taxas...";
+
+            var connection = networkService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                
+                LoadLocalRates();
+                load=false;
+            }
+            else
+            {
+                //Carrega os dados da api
+                await LoadApiRates();
+                load = true;
+            }
+
+            if (Rates.Count == 0)
+            {
+                LabelResultado.ForeColor = Color.Red;
+                LabelStatus.ForeColor = Color.Red;
+                LabelResultado.Text = "Não há ligação á Internet" + Environment.NewLine +
+                    "e não foram prévimente carregadas as taxas." + Environment.NewLine +
+                    "Tente mais tarde! ";
+                LabelStatus.Text = "Primeira inicialização deverá ter a ligação á Internet.";
+                return;
+            }
+            ComboBoxOrigem.DataSource = Rates;
+            ComboBoxOrigem.DisplayMember = "Name";
+
+            //BindingContext é a Classe que nos liga os objetos do interface ao codigo
+            //Corrige bug da microsoft -- faz o binding de duas combobox diferentes
+            ComboBoxDestino.BindingContext=new BindingContext();
+            ComboBoxDestino.DataSource = Rates;
+            ComboBoxDestino.DisplayMember = "Name";
+
+           
+            ButtonConverter.Enabled = true;
+            ButtonTroca.Enabled = true;
+            LabelResultado.Text = "Taxas Atualizadas...";
+
+            if (load)
+            {
+                LabelStatus.Text = string.Format("Taxas carregadas da internet em {0:F}", DateTime.Now);
+            }
+            else
+            {
+                LabelStatus.Text = string.Format("Taxas carregadas da Base de Dados ");
+
+            }
+            ProgressBar1.Value = 100;
+        }
+
+        private void LoadLocalRates()
+        {
+            Rates=dataService.GetData();
+        }
+
+        private async Task LoadApiRates()
+        {
+            ProgressBar1.Value = 0;
+            var response = await apiService.GetRates("https://cambiosrafa.azurewebsites.net", "/api/rates");
+            Rates =(List<Rate>) response.Result;
+
+            dataService.DeleteData();
+            dataService.SaveData(Rates);
+        }
+
+        private void ButtonConverter_Click(object sender, EventArgs e)
+        {
+            Converter();
+        }
+
+        private void Converter()
+        {
+            if (string.IsNullOrEmpty(TextBoxValor.Text))
+            {
+                dialogService.ShowMessage("Erro", "Insira um valor a converter");
+                return;
+            }
+            decimal valor;
+            if (!decimal.TryParse(TextBoxValor.Text, out valor))
+            {
+                dialogService.ShowMessage("Erro de conversão","Valor terá que ser numérico");
+                return;
+            }
+            if (ComboBoxOrigem.SelectedItem==null)
+            {
+                dialogService.ShowMessage("Erro","Tem que escolher uma moeda a converter");
+                return;
+            }
+            if (ComboBoxDestino.SelectedItem == null)
+            {
+                dialogService.ShowMessage("Erro", "Tem que escolher uma moeda de destino para converter");
+                return;
+            }
+
+            var taxaOrigem =(Rate) ComboBoxOrigem.SelectedItem;
+            var taxaDestino = (Rate)ComboBoxDestino.SelectedItem;
+           
+            var valorConvertido= valor/(decimal) taxaOrigem.TaxRate* (decimal) taxaDestino.TaxRate;
+
+            LabelResultado.Text = string.Format("{0}  {1:F2} = {2} {3:F2}", taxaOrigem.Code, valor, taxaDestino.Code, valorConvertido);
+
+        }
+
+        private void ButtonTroca_Click(object sender, EventArgs e)
+        {
+            Trocar();
+        }
+
+        private void Trocar()
+        {
+            var aux = ComboBoxOrigem.SelectedItem;
+
+            ComboBoxOrigem.SelectedItem = ComboBoxDestino.SelectedItem;
+            ComboBoxDestino.SelectedItem = aux;
+            Converter();
+        }
+
+        private void TextBoxValor_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBoxValor.Text))
+            {
+                Converter();
+            }
+           
+        }
+    }
+}
